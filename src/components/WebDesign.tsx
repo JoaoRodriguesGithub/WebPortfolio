ffc21c2e31a59fe61cdabc1b01b6a20c6c558ef6
JR-LAPTOP01\JR <a20629@alunos.ipca.pt>
2023-04-02 18:43:19 +0100

    Adding aditional threejs objects

---
+++ src/components/WebDesign.tsx
@@ -0,0 +1,16 @@
+import { OrbitControls, Stage } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import Mac from "./Mac";
+
+const WebDesign = () => {
+  return (
+    <Canvas>
+      <Stage environment="city" intensity={0.6}>
+        <Mac />
+      </Stage>
+      <OrbitControls enableZoom={false} />
+    </Canvas>
+  );
+};
+
+export default WebDesign;
--- src/components/Works.tsx
+++ src/components/Works.tsx
@@ -1,4 +1,8 @@
+import { useState } from "react";
 import styled from "styled-components";
+import WebDesign from "./WebDesign";
+import Development from "./Development";
+import ProductDesign from "./ProductDesign";
 
 interface ListItemProps {
   text: string;
@@ -71,19 +75,28 @@ const Right = styled.div`
 `;
 
 const Works = () => {
+  const [work, setWork] = useState("Web Design");
   return (
     <Section>
       <Container>
         <Left>
           <List>
             {data.map((item) => (
-              <ListItem key={item} text={item}>
+              <ListItem key={item} text={item} onClick={() => setWork(item)}>
                 {item}
               </ListItem>
             ))}
           </List>
         </Left>
-        <Right></Right>
+        <Right>
+          {work === "Web Design" ? (
+            <WebDesign />
+          ) : work === "Development" ? (
+            <Development />
+          ) : (
+            <ProductDesign />
+          )}
+        </Right>
       </Container>
     </Section>
   );
